f189ffc8985567a89bd5184babc165d2d9e56d41
armnit <armnit@localhost.localdomain>
2015-09-01 17:32:58 +0200

    v4/test

--- Symfony/README.md
+++
@@ -1,4 +0,0 @@
-Symfony
-=======
-
-A Symfony project created on September 1, 2015, 11:43 am.
--- Symfony/src/IEP/InscriptionBundle/IEPInscriptionBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace IEP\InscriptionBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class IEPInscriptionBundle extends Bundle
-{
-}
